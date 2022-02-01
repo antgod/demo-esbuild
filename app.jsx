24938fdb7dbfdd4c7aceb4b96e942f4e0847c4a0
hongji.lhj <hongji.lhj@alibaba-inc.com>
2022-02-01 15:20:19 +0800

    feat: 初始化

---
+++ app.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './home'
+
+const App = () => {
+  return <Home />
+}
+
+ReactDOM.render(<App />, document.getElementById('div'))
---
+++ home.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export default () => {
+  return <h1>good mornning world, great!</h1>
+}
